f95336836a59d39d17b20124819dc79e7afe4e33
Raj <raj.21910831@viit.ac.in>
2022-05-07 12:49:23 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios'
+import Coin from './Coin';
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
 
 function App() {
+  const [Coins, setCoins] = useState([]);
+  const [Search, setSearch] = useState('')
+  useEffect(() =>{
+     axios
+     .get(url)
+     .then((res)=>{
+        setCoins(res.data)
+      //  console.log(Coins);  
+     })
+     .catch((err)=>{
+       console.log(err);
+     })
+  }, [])
+  const handleChange=(e)=>{
+    setSearch(e.target.value);
+  }
+  const fillCoins= Coins.filter( coin=> coin.name.toLowerCase().includes(Search.toLowerCase()));
+  console.log(fillCoins);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="coin-app">
+       <div className="coin-search">
+         <h1 className="coin-text">
+           Search a currency 
+         </h1>
+         <form>
+           <input type="text" placeholder='Search' className='coin-input' onChange={handleChange}/>
+         </form>
+         <div className='titles'>
+           <p>Name</p>
+           <p>Symbol</p>
+           <p>Price</p>
+           <p>Volume</p>
+           <p>% change</p>
+           <p>Market Cap</p>
+         </div>
+       </div>
+       
+       {fillCoins.map( ele => {
+         return(
+           
+          <Coin key={ele.id}
+           image={ele.image}
+           symbol={ele.symbol} 
+           name={ele.name} 
+           price={ele.current_price} 
+           volume={ele.total_volume} 
+           priceChange = {ele.price_change_percentage_24h}
+           marketcap = {ele.market_cap}
+           />
+         )
+       })}
+    </div>      
+    </>
   );
 }
 
